7e31c299b10c7c92f9ce537cd20164907a4dd1aa
whiteworker <452568821@qq.com>
2020-11-27 12:08:05 +0800

    fix

---
+++ src/gomst/channel/close/close.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func read(ch1 chan int,ch2 chan bool){
+    for{       
+        // v,ok := <-ch1
+        // if ok{
+        //     fmt.Printf("read a int is %d\n",v)
+        // } else{
+        //     ch2 <-true
+        // }
+        select{
+            case v,ok :=<-ch1:
+                if ok{
+                    fmt.Printf("read a int is %d\n",v)
+                } else{
+                    ch2 <- true
+                }
+            default:
+                ch2<-false
+        }
+     }
+    
+}
+
+func write(ch chan int){
+    for i:=0;i<10;i++{
+         ch <- i
+    }
+    close(ch)
+}
+
+func main() {
+    ch1 := make(chan int)
+    ch2 := make(chan bool)
+
+    go write(ch1)
+    go read(ch1,ch2)
+
+    <-ch2
+}
