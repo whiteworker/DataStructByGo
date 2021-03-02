4af357c52050794969f298244a8039bdda773494
whiteworker <452568821@qq.com>
2021-03-02 22:27:25 +0800

    lru

--- src/base/heap_sort/heap_sort.go
+++ src/base/heap_sort/heap_sort.go
@@ -8,16 +8,17 @@ func HeapSort(array []int) {
 	m := len(array)
 	s := m/2
 	for i := s; i > -1; i-- {
+		//从第一个非叶子结点从下至上，从右至左调整结构
 		heap(array, i, m-1)
 	}
 	for i := m-1; i > 0; i-- {
-		array[i], array[0] = array[0], array[i]
-		heap(array, 0, i-1)
+		array[i], array[0] = array[0], array[i]//将堆顶元素与末尾元素进行交换
+		heap(array, 0, i-1)//重新对堆进行调整
 	}
 }
 
 func heap(array []int, i, end int){
-	l := 2*i+1
+	l := 2*i+1 
 	if l > end {
 		return
 	}
