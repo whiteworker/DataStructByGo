6aaaabd938ca428280e4fbc2f9537930d3a658b9
whiteworker <452568821@qq.com>
2021-03-03 19:27:51 +0800

    add 前序遍历

--- hotleetcode/11二叉树的层序遍历/main.go
+++ hotleetcode/11二叉树的层序遍历/main.go
@@ -12,6 +12,22 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func preorderTraversal(root *TreeNode) (vals []int) {
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			vals = append(vals, node.Val)
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1].Right
+		stack = stack[:len(stack)-1]
+	}
+	return
+}
+
+//前序遍历
 func levelOrder(root *TreeNode) [][]int {
 	res := [][]int{}
 	if root == nil {
