f247956219a5a0ca1416a950027c8b7decefa5b6
whiteworker <452568821@qq.com>
2021-03-09 01:10:18 +0800

    aa

--- src/base/sort3way/sort3way.go
+++ src/base/sort3way/sort3way.go
@@ -1,30 +1,26 @@
-package main
+package sort3way
 
-//在lt之前的(lo~lt-1)都小于中间值
-//在gt之前的(gt+1~hi)都大于中间值
+//在lt之前的(start~lt-1)都小于中间值
+//在gt之前的(gt+1~end)都大于中间值
 //在lt~i-1的都等于中间值
 //在i~gt的都还不确定（最终i会大于gt，即不确定的将不复存在）
-func Sort3way(s []int, lo, hi int) {
-	if lo >= hi {
+func Sort3way(s []int, start, end int) {
+	if start >= end {
 		return
 	}
-	v, lt, i, gt := s[lo], lo, lo+1, hi
+	v, lt, i, gt := s[start], start, start+1, end
 	for i <= gt {
 		if s[i] < v {
-			swap(s, i, lt)
+			s[i], s[lt] = s[lt], s[i]
 			lt++
 			i++
 		} else if s[i] > v {
-			swap(s, i, gt)
+			s[i], s[gt] = s[gt], s[i]
 			gt--
 		} else {
 			i++
 		}
 	}
-	Sort3way(s, lo, lt-1)
-	Sort3way(s, gt+1, hi)
-}
-
-func swap(s []int, i int, j int) {
-	s[i], s[j] = s[j], s[i]
+	Sort3way(s, start, lt-1)
+	Sort3way(s, gt+1, end)
 }
