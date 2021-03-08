a7439cee209e80989e2a6f09ed53ba8e76c0110e
zlbai <zlbai@trip.com>
2021-03-08 17:14:52 +0800

    add

---
+++ big/mq/mq2channel/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Producer(cdata chan int, step int) {
+	for i := 0; ; i += step {
+		cdata <- i
+	}
+}
+func Consumer(cdata chan int) {
+	for {
+		if v, ok := <-cdata; ok {
+			fmt.Println(v)
+		}
+	}
+}
+func main() {
+	cdata := make(chan int, 10)
+	go Producer(cdata, 2)
+	go Producer(cdata, 3)
+	go Consumer(cdata)
+	time.Sleep(time.Second * 5)
+}
