252e85eb175e85b59939e8ec59c1076b389548e1
whiteworker <452568821@qq.com>
2021-03-09 01:01:45 +0800

    ss

--- src/base/sort3way/sort3way.go
+++ src/base/sort3way/sort3way.go
@@ -1,38 +1,30 @@
-package sort3way
+package main
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
- func nthUglyNumber(n int) int {
-    dp := make([]int,n)
-    dp[0]=1
-    a,b,c :=0,0,0
-    for i:=1 ;i<n;i++{
-        numA := dp[a]*2
-        numB := dp[b]*2
-        numC := dp[c]*2
-        dp[i]=min( min(numA,numB),numC)
-        if dp[i]==numA{
-            a++
-        }
-        if dp[i]==numB{
-            b++
-        }
-        if dp[i]==numC{
-            c++
-        }
-    }
-    return dp[n-1]
+//在lt之前的(lo~lt-1)都小于中间值
+//在gt之前的(gt+1~hi)都大于中间值
+//在lt~i-1的都等于中间值
+//在i~gt的都还不确定（最终i会大于gt，即不确定的将不复存在）
+func Sort3way(s []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	v, lt, i, gt := s[lo], lo, lo+1, hi
+	for i <= gt {
+		if s[i] < v {
+			swap(s, i, lt)
+			lt++
+			i++
+		} else if s[i] > v {
+			swap(s, i, gt)
+			gt--
+		} else {
+			i++
+		}
+	}
+	Sort3way(s, lo, lt-1)
+	Sort3way(s, gt+1, hi)
 }
-func min(a,b int)int{
-    if a>b{
-        return b
-    } else{
-        return a
-    }
+
+func swap(s []int, i int, j int) {
+	s[i], s[j] = s[j], s[i]
 }
