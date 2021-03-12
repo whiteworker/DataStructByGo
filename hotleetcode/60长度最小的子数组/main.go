e6568acf185f57e2400aea71f38b333ec20942e2
whiteworker <452568821@qq.com>
2021-03-12 10:47:45 +0800

    aa

---
+++ hotleetcode/60长度最小的子数组/main.go
@@ -0,0 +1,28 @@
+unc minSubArrayLen(s int, nums []int) int {
+    n := len(nums)
+    if n == 0 {
+        return 0
+    }
+    ans := math.MaxInt32
+    for i := 0; i < n; i++ {
+        sum := 0
+        for j := i; j < n; j++ {
+            sum += nums[j]
+            if sum >= s {
+                ans = min(ans, j - i + 1)
+                break
+            }
+        }
+    }
+    if ans == math.MaxInt32 {
+        return 0
+    }
+    return ans
+}
+
+func min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
